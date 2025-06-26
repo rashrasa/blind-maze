493ab1cf0be93120b9e50651be541c89b31f51d6
rashrasa <ahmed.rashrash@gmail.com>
2025-06-25 20:58:26 -0400

    Walking skeleton for game client

--- src/app/lib/client/game_client.tsx
+++ src/app/lib/client/game_client.tsx
@@ -1,9 +1,53 @@
-'use client';
+"use client";
+import React, { ReactNode, useState } from "react";
 
-export default function GameClient() {
+enum GameClientMenu {
+    MAIN_MENU,
+    SETTINGS_MENU,
+    GAME_SCREEN,
+}
+
+interface GameClientState {
+    menu: GameClientMenu;
+    server: String | null;
+}
+
+interface GameClientProps { }
+
+
+const GameClient: React.FC<GameClientProps> = () => {
+    const [state, setState] = useState<GameClientState>({
+        menu: GameClientMenu.MAIN_MENU,
+        server: null,
+    });
+    switch (state.menu) {
+        case GameClientMenu.MAIN_MENU:
+            return buildMainMenu();
+        case GameClientMenu.SETTINGS_MENU:
+            return buildSettingsMenu();
+        case GameClientMenu.GAME_SCREEN:
+            return buildGameScreen(state.server!);
+    }
+};
+
+
+
+function buildMainMenu(): ReactNode {
     return (
         <div>
-            
+            <p>
+            </p>
         </div>
-    );
+    )
 }
+
+function buildSettingsMenu(): ReactNode {
+    return <div></div>;
+}
+
+function buildGameScreen(server: String): ReactNode {
+
+    return <div></div>;
+}
+
+export default GameClient;
