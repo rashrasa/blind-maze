3789ea5b62f0ee19408368453dfe739cf7b3af72
rashrasa <ahmed.rashrash@gmail.com>
2025-08-24 14:18:08 -0400

    server: Created hello world WebSocket server in Go

--- apps/go-server/main.go
+++ apps/go-server/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type WebsocketHandler struct {
+	upgrader websocket.Upgrader
+}
+
+func (wsh WebsocketHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	conn, err := wsh.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("New connection from " + r.RemoteAddr)
+	for {
+		messageType, bytes, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println("Received message: " + string(bytes))
+		conn.WriteMessage(messageType, []byte("Hello, World"))
+	}
+}
 
 func main() {
-	fmt.Println("test")
+	webSocketHandler := WebsocketHandler{
+		upgrader: websocket.Upgrader{},
+	}
+	http.Handle("/", webSocketHandler)
+	log.Println("Starting server...")
+
+	log.Fatal(http.ListenAndServe("localhost:3001", nil))
 }
