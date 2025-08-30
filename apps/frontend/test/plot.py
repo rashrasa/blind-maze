15749e8b9475a16f7323fe7963167e47e15300cb
rashrasa <ahmed.rashrash@gmail.com>
2025-08-30 16:20:57 -0400

    removed old tests

--- apps/frontend/test/plot.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plot
-import pandas as pd
-
-dataFrame = pd.read_csv("./test/output/output.csv")
-
-dataFrame.plot.scatter(x="seedLength", y="result")
-    
-
-plot.show()
